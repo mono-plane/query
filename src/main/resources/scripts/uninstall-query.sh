ab86aa4fb70c952198914574adb584e6acbf27b9
Heiko Braun <ike.braun@googlemail.com>
2015-03-24 15:43:58 +0100

    Fix #4: package the presto-cli

--- src/main/resources/scripts/uninstall-query.sh
+++ src/main/resources/scripts/uninstall-query.sh
@@ -3,6 +3,8 @@
 set -o xtrace
 
 rm -rf modules/system/layers/base/org/wildfly/extension/monoplane/query/
+rm -rf modules/system/layers/base/org/wildfly/extension/monoplane/cli/
 rm -f standalone/configuration/standalone-query.xml
 rm -f domain/configuration/query-domain.xml
 rm -f domain/configuration/query-host.xml
+rm -f bin/mono-cli.sh
